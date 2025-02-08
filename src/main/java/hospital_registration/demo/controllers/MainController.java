82ecbc3bafd65bd204872d7e36f347b4641f2e04
Stas <stastokaruk1605@gmail.com>
2025-02-08 17:07:51 +0200

    native structure 2.0

---
+++ src/main/java/hospital_registration/demo/controllers/MainController.java
@@ -0,0 +1,24 @@
+package hospital_registration.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+/**
+ * Controller for handling requests to the main page.
+ */
+@Controller
+public class MainController {
+
+    /**
+     * Displays the home page.
+     *
+     * @param model the model to add attributes to
+     * @return the name of the home view
+     */
+    @GetMapping("/home")
+    public String greeting(Model model) {
+        return "home";
+    }
+}
