6f5952db3af36ddbb6e8d211b5c1fad4a67098a5
Stas <stastokaruk1605@gmail.com>
2025-02-08 16:33:13 +0200

    v0.0

---
+++ src/main/java/hospital_registration/demo/HospitalRegApplication.java
@@ -0,0 +1,13 @@
+package hospital_registration.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalRegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalRegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hospital_registration/demo/HospitalRegApplicationTests.java
@@ -0,0 +1,13 @@
+package hospital_registration.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
